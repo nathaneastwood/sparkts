2e2299d6c4187dffd6af84751bd7242878a05937
nathaneastwood <neastwood@mango-solutions.com>
2018-03-01 16:07:38 +0000

    refactor: Remove the functionalised version of the sdf_standard_error function in favour of the R6 Class version

--- R/sdf_standard_error.R
+++ R/sdf_standard_error.R
@@ -3,87 +3,20 @@
 #' This function will add an extra column on to a Spark DataFrame containing the
 #' standard error.
 #'
-#' @param sc A \code{spark_connection}.
-#' @param data The Spark \code{DataFrame} on which to perform the function.
-#' @param x_col The column to be used as X in the calculation.
-#' @param y_col The column to be used as Y in the calculation.
-#' @param z_col The column to be used as Z in the calculation.
-#' @param new_column_name The name of the standard error column.
-#'
-#' @examples
-#' # Set up a spark connection
-#' sc <- sparklyr::spark_connect(master = "local", version = "2.1.0")
-#'
-#' # Extract some data
-#' std_data <- sparklyr::spark_read_json(
-#'   sc,
-#'   "std_data",
-#'   path = system.file(
-#'     "data_raw/StandardErrorDataIn.json",
-#'     package = "sparkts"
-#'   )
-#' ) %>%
-#'   sparklyr::spark_dataframe()
-#'
-#' # Calculate the standard error
-#' output <- sdf_standard_error(
-#'   sc,
-#'   std_data,
-#'   x_col = "xColumn",
-#'   y_col = "yColumn",
-#'   z_col = "zColumn",
-#'   new_column_name = "stdError"
-#' )
-#'
-#' # Extract the standard error column
-#' output <- output %>% dplyr::collect()
-#' output[["stdError"]]
-#'
-#' sparklyr::spark_disconnect(sc = sc)
-#'
-#' @importFrom sparklyr invoke_static invoke
-#' @importFrom dplyr collect %>%
-#'
-#' @export
-sdf_standard_error <- function(
-  sc, data, x_col, y_col, z_col, new_column_name
-) {
-
-  invoke_static(
-    sc,
-    class = "com.ons.sml.businessMethods.methods.StandardError",
-    method = "standardError",
-    df = data
-  ) %>%
-    invoke(
-      method = "stdErr1",
-      df = data,
-      xCol = x_col,
-      yCol = y_col,
-      zCol = z_col,
-      newColName = new_column_name
-    )
-
-}
-
-#' Calculate the standard error
-#'
-#' This function will add an extra column on to a Spark DataFrame containing the
-#' standard error.
-#'
 #' @section Usage:
 #' \preformatted{
-#'   p <- sdf_standard_error_r6$new(sc = sc, data = std_data)
-#'   p$standard_error(
-#'     x_col = "xColumn", y_col = "yColumn", z_col = "zColumn",
-#'     new_column_name = "test"
-#'   )
+#'   p <- sdf_standard_error$new(sc, data)
+#'   p$standard_error(data = NULL, x_col, y_col, z_col, new_column_name)
 #' }
 #'
 #' @section Arguments:
 #' \describe{
-#'   \item{p}{A \code{waterflow} object.}
-#'   \item{wall}{A numeric vector of wall heights}
+#'   \item{p}{An \code{sdf_standard_error} object.}
+#'   \item{x_col}{A string. The column to be used as X in the calculation.}
+#'   \item{y_col}{A string. The column to be used as Y in the calculation.}
+#'   \item{z_col}{A string. The column to be used as Z in the calculation.}
+#'   \item{new_column_name}{A string. This is what the standard error column is
+#'     called, it can be defaulted to "StandardError".}
 #' }
 #'
 #' @section Details:
@@ -94,7 +27,6 @@ sdf_standard_error <- function(
 #'
 #' @importFrom R6 R6Class
 #'
-#' @name sdf_standard_error_r6
 #' @examples
 #' # Set up a spark connection
 #' sc <- sparklyr::spark_connect(master = "local", version = "2.1.0")
@@ -111,16 +43,20 @@ sdf_standard_error <- function(
 #'   sparklyr::spark_dataframe()
 #'
 #' # Instantiate the class
-#' p <- sdf_standard_error_r6(sc = sc, data = std_data)
+#' p <- sdf_standard_error$new(sc = sc, data = std_data)
 #' # Calculate the standard errors
 #' p$standard_error(
 #'   x_col = "xColumn", y_col = "yColumn", z_col = "zColumn",
 #'   new_column_name = "test"
 #' )
+#'
+#' @name sdf_standard_error
+#'
+#' @export
 NULL
 
-#' @export
-sdf_standard_error_r6 <- R6::R6Class(
+#' @importFrom R6 R6Class
+sdf_standard_error <- R6::R6Class(
   "sdf_standard_error",
   inherit = utils,
   public = list(
